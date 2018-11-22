3588b88b274b69953ca98f740479c7148e86cdc3
Roy van Santen <roy.van.santen@ing.nl>
2018-11-22 16:50:05 +0100

    Start of part 4

--- huffman/load_tree.c
+++ huffman/load_tree.c
@@ -3,7 +3,16 @@
 
 tree_t load_tree(char* input) {
 
-    // ... SOME CODE MISSING HERE ...
+    struct stack* stack = stack_init();
+
+    while (*input++) {
+        stack_push(stack, *input);
+    }
+    
+    while (stack_peek(stack)) {
+        char* c = stack_pop(stack);
+        
+    }
 
     return 0;
 }
