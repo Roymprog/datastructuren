87ae1f300f6f1d907e73f8d97977767e61308ddf
Roy van Santen <royvsanten@gmail.com>
2018-11-25 17:01:43 +0100

    Rephrasing comments

--- huffman/print_code.c
+++ huffman/print_code.c
@@ -6,12 +6,12 @@ int print_code(code c) {
 		return -1;
 	}
 
-	// There is at least 1 character in the binary code
+	// There is at least 1 number in the "binary integer"
 	int bin_character_count = 1;
 	int number = c.bin_encoding;
 
-	// Rather savage way of counting the amount of characters in "binary" integer,
-	// loops cuts off last number of "binary" integer each iteration
+	// Rather savage way of counting the amount of characters in "binary integer",
+	// loops cuts off last number of "binary integer" each iteration
 	while((number = number / 10)) {
 		bin_character_count++;
 	} 
